31d5e6ef2248901985494d3739119c2bdde1b8a9
William Schultz <will627@comcast.net>
2022-02-07 21:56:16 -0500

    Save progress

--- pset01_ProgramAnalysis/Pset1Implementation.v
+++ pset01_ProgramAnalysis/Pset1Implementation.v
@@ -598,68 +598,75 @@ Module Impl.
   Qed.
 
   (* If validate returns true, then runPortable should return true. *)
-  Lemma validate_sound_bool : forall p, validate p = true ->
+  Lemma validate_sound_fst : forall p, validate p = true ->
     (forall s, fst (runPortable p s) = true).
     simplify. apply symbolicEval_implies_validate in H as HN.
     apply symbolicEval_sound. equality. 
   Qed.
 
-  (* Lemma runPortable_run : 
+  (* Lemma runPortable_run : forall p s0 s1, *)
+  (*  runPortable p s0 = (true, s1) -> run p s0 = s1. *)
+
+  (* Lemma runPortable_run_tup : 
         forall p s0 s1, 
-        runPortable p s0 = (true, s1) -> run p s0 = s1. *)
-        
-  (* Lemma runPortable_run_aux :  
+        ((fst (runPortable p s0) = true) -> (snd (runPortable p s0) = s1)) <-> runPortable p s0 = (true, s1).
+        simplify.
+        split.
+        cases (fst (runPortable p s0)).
+            - simplify. rewrite tup_eq. f_equal. equality. equality.
+            - simplify. rewrite tup_eq. f_equal.
+        - rewrite tup_eq. simplify.
+        (* (run p s0) = s1. *)
+        simplify.
+    Qed. *)
+
+  Lemma runPortable_run_aux :  
     forall p s0 s1,   
-    (fst (runPortable p s0) = true) -> (snd (runPortable p s0) = s1).
+        (fst (runPortable p s0) = true) -> (snd (runPortable p s0) = s1) <-> (run p s0) = s1.
     simplify.
-    cases (runPortable p s0).
+    split.
+    apply runPortable_run.
+    cases (runPortable p s).
+    rewrite pair_equal_spec. split.
+    equality.
+    equality.
+  Qed.
+
+  (* Lemma runPortable_run_aux2 :  
+  forall p s,   
+      (fst (runPortable p s) = true) -> (snd (runPortable p s) = (run p s)).
+  simplify.
+  apply runPortable_run_aux in H.
+  split.
+  apply runPortable_run_aux.
+  apply runPortable_run.
+  cases (runPortable p s).
+  rewrite pair_equal_spec. split.
+  equality.
+  equality.
   Qed. *)
 
-  Lemma validate_sound_result : forall p, validate p = true ->
-    forall s, snd (runPortable p s) = (run p s).
-    apply validate_sound_bool.
 
-    trivial.
-    rewrite tup_eq.
-    apply validate_sound_bool in H.
-    cases (runPortable p s).
-    (* apply validate_sound_bool in H. *)
-    rewrite tup_eq.
-
-    cases (runPortable p s).
+  (* TODO: This is the remaining lemma that need to be proven. *)
+  Lemma validate_sound_snd : 
+    forall p, validate p = true ->
+        forall s, snd (runPortable p s) = (run p s).
     simplify.
-    rewrite tup_eq in Heq.
-    rewrite validate_sound_bool in Heq.
-    cases b.
-    - symmetry.
-      apply runPortable_run. equality.
-    (* - rewrite tup_eq in Heq. f_equal.  *)
-    (* apply validate_sound_bool in H. *)
-    (* rewrite tup_eq in Heq. *)
-    (* rewrite validate_sound_bool in Heq. *)
-    (* apply symbolicEval_implies_validate in H. *)
-    (* apply symbolicEval_sound. equality. *)
+    apply symbolicEval_implies_validate in H.
+    apply symbolicEval_sound with (s:=s) in H.
+    rewrite runPortable_run_aux in H.
+    (* apply runPortable_run in H0. *)
+    (* simplify. equality. *)
   Admitted.
-
-  (* runPortable_run *)
 
   Lemma validate_sound : forall p, validate p = true ->
     forall s, runPortable p s = (true, run p s).
     simplify.
-    (* rewrite tup_eq.  *)
-    apply symbolicEval_implies_validate in H.
-    apply symbolicEval_sound  with (s:=s) in H.
-    apply runPortable_run.
-
-    rewrite pair_equal_spec. 
+    rewrite tup_eq. 
+    rewrite pair_equal_spec.
     split.
-    apply symbolicEval_implies_validate in H.
-    rewrite symbolicEval_sound. equality. equality.
-    apply symbolicEval_implies_validate in H.
-    apply symbolicEval_sound in H.
-
-    (* simplify. apply symbolicEval_implies_validate in H as HN. *)
-    (* apply symbolicEval_sound. equality.  *)
+    apply validate_sound_fst. equality.
+    apply validate_sound_snd. equality.
   Qed.
 
   (* Here is the complete list of commands used in one possible solution:
